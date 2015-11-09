cee9e3ed0a83880a8dd1a689ab562831a444c86e
Jason Williams <jase.williams@gmail.com>
2015-11-09 15:05:43 +0000

    some updates to monitoring handler,500 and 404 have their own metric

--- src/MonitoringHandler.php
+++ src/MonitoringHandler.php
@@ -26,15 +26,19 @@ class MonitoringHandler
     protected $client;
     protected $namespace;
 
-    public function __construct(CloudWatchClient $client, $namespace)
+    public function __construct(CloudWatchClient $client, $namespace, $env)
     {
         $this->client = $client;
         $this->namespace = 'BBCApp/' . $namespace;
+        /* From what i could see the PHP SDK doesn't provide you with the instance-id so you need to get it yourself */
+        $this->instanceID =  file_get_contents("http://instance-data/latest/meta-data/instance-id");
+        $this->env = $env;
     }
 
     /**
      * Generic function which will put metric data, and act as the lower level function for most calls in this class
      * CloudWatchClient->putMetricData already uses promises under the hood, so no need to worry about these requests being async - http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/promises.html
+     * This function will add on the environment and the instance-d to the dimensions passed in
      *
      * @return  void
      * @param string $metricName Metricname
@@ -44,6 +48,11 @@ class MonitoringHandler
     */
     public function putMetricData($metricName, $value, $dimensions)
     {
+        /* append data to $dimensions so it doesn't need to be done every time */
+        $dimensions[] = array('Name' => 'BBCEnvironment', 'Value' => $this->env);
+        $dimensions[] = array('Name' => 'instanceId', 'Value' => $this->instanceID);
+
+        /* Build metric */
         $this->client->putMetricData(array(
             'Namespace' => $this->namespace,
             'MetricData' => array(
@@ -86,7 +95,7 @@ class MonitoringHandler
     * This is a dimension for application errors, this is a 500 error within the application will live in here
     */
     public function application500Error() {
-        $this->putMetricData('applicationError', 1, array(array('Name' => 'error', 'Value' => '500')));
+        $this->putMetricData('Http500Response', 1, array());
     }
 
 
@@ -94,14 +103,14 @@ class MonitoringHandler
     * This is an dimensions for 404 on application errors, all errors within the application will live in here
     */
     public function application404Error() {
-        $this->putMetricData('applicationError', 1, array(array('Name' => 'error', 'Value' => '404')));
+        $this->putMetricData('Http404Response', 1, array());
     }
 
     /**
     * This is a dimension for application errors, this is a generic catch-all within the application will live in here
     */
     public function applicationError() {
-        $this->putMetricData('applicationError', 1, array(array('Name' => 'error', 'Value' => 'AppError')));
+        $this->putMetricData('applicationError', 1, array());
     }
 
     /**
